dbb14f469f26656f390efe5b9926273f56a73c39
Kyle Huynh <kyle.huynh205@gmail.com>
2022-03-22 14:33:14 -0400

    added microservices and access control part

---
+++ scripts/access_control.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+#inital_path
+inital_path=$PWD
+
+#current site
+site_path="${inital_path}"/..
+
+public_files_path="${site_path}"/web/sites/default/files
+private_files_path="${site_path}"/web/sites/default/private_files
+
+#Enable access control modules
+drush -y pm:enable group groupmedia group_permissions gnode islandora_group_defaults media_library_edit
+
+# enable islandora access control with group
+drush -y pm:enable islandora_group group_solr
+
+# import group_permissions
+drush -y config-import --partial --source=$"${inital_path}"/config/group_permission
+
+mkdir "${private_files_path}"
+#chown -Rf www-data:www-data "${private_files_path}"
+
+# update settings.php with path of private file system
+chmod 777 "${site_path}"/web/sites/default/settings.php 
+cd "${site_path}"/web/sites/default && sed -i "/file_private_path/c\$settings['file_private_path'] = 'sites/default/private_files';" settings.php && chmod 444 settings.php && cd "${inital_path}"
+
+# configure file system
+drush -y config-import --partial --source=$"${inital_path}"/config/private_file_system/system
+
+# configure media's file fields
+drush -y config-import --partial --source=$"${inital_path}"/config/private_file_system/media
+
+
+# Apply patch for file_entity
+wget https://raw.githubusercontent.com/digitalutsc/override_permission_file_entity/main/override_file_access.patch -P "${site_path}"/web/modules/contrib/file_entity
+cd "${site_path}"/web/modules/contrib/file_entity && patch -p1 < override_file_access.patch && cd "${inital_path}"
+
+
+# import access control fields
+drush -y config-import --partial --source=$PWD/config/access_control
+
+# import config for access controle field with taxonomy terms
+drush -y config-set --input-format=yaml islandora_group.config collection_based "islandora_access"
+drush -y config-set --input-format=yaml islandora_group.config role_based "islandora_access"
+drush -y config-set --input-format=yaml islandora_group.config node-type-access-fields '{"article":"field_access_terms","islandora_object":"field_access_terms","page":"field_access_terms"}'
+drush -y config-set --input-format=yaml islandora_group.config media-type-access-fields '{"audio":"field_access_terms","document":"field_access_terms","file":"field_access_terms","image":"field_access_terms","remote_video":"field_access_terms","video":"field_access_terms","web_archive":"field_access_terms"}'
+
+# enable default values for access control terms 
+drush -y pm:enable field_access_terms_defaultvalue
+
+# disable setup modules
+drush -y pmu islandora_group_defaults field_access_terms_defaultvalue
---
+++ scripts/micro_services.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+ #inital_path
+inital_path=$PWD
+
+#current site
+site_path=$PWD/..
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    exit 0
+fi
+
+if [ $1 == "playbook" ]; then
+    #configure_search_api_solr_module
+    SOLR_CORE=ISLANDORA
+    solr_host=localhost:8983
+    solr_core=multisite
+    cantaloupe_url=http://localhost:8080/cantaloupe/iiif/2
+    
+    #blazegraph
+    blazegraph_url=http://localhost:8080/bigdata
+    blazegraph_namespace=islandora
+    
+    #fits
+    fits_mode="remote"
+    fits_url=http://localhost:8080/fits/examine
+    fits_config_var="fits-server-url"
+        
+elif [ $1 == "docker" ]; then
+    #configure_search_api_solr_module
+    SOLR_CORE=ISLANDORA
+    solr_host=islandora.traefik.me:8983
+    solr_core=multisite
+
+    #iiif server
+    cantaloupe_url=https://islandora.traefik.me/cantaloupe
+
+    #blazegraph
+    blazegraph_url=https://islandora.traefik.me:8082/bigdata
+    blazegraph_namespace=islandora
+
+    #fits
+    fits_mode="local"
+    fits_url=/opt/fits-1.4.1/fits.sh
+    fits_config_var="fits-path"
+    
+    # Setup Fits
+    mkdir -p /opt/fits
+    wget https://github.com/harvard-lts/fits/releases/download/1.4.0/fits-latest.zip -P /opt/fits
+    unzip /opt/fits/fits-latest.zip
+else
+  echo "Please enter which environment you running this script on"
+  exit 0
+fi
+
+
+#Enable microservice modules
+drush -y pm:enable advancedqueue_runner triplestore_indexer fits
+
+# configure Advanced Queue
+drush -y config-import --partial --source=$PWD/config/advanced_queue
+
+# configure advanced queue runner
+drush -y config-set --input-format=yaml advancedqueue_runner.runnerconfig drush_path "${site_path}"/vendor/drush/drush/drush
+drush -y config-set --input-format=yaml advancedqueue_runner.runnerconfig root_path "${site_path}"
+drush -y config-set --input-format=yaml advancedqueue_runner.runnerconfig auto-restart-in-cron 1
+drush -y config-set --input-format=yaml advancedqueue_runner.runnerconfig queues "
+- default: default
+- triplestore: triplestore
+- fits:fits
+"
+drush -y config-set --input-format=yaml advancedqueue_runner.runnerconfig interval '5'
+drush -y config-set --input-format=yaml advancedqueue_runner.runnerconfig mode limit
+
+# Configure Rest Services (enable jsonld endpoint)
+drush -y config-import --partial --source=$PWD/config/rest
+
+#configure triplestore_indexer
+drush -y config-set --input-format=yaml triplestore_indexer.triplestoreindexerconfig server-url "${blazegraph_url}"
+drush -y config-set --input-format=yaml triplestore_indexer.triplestoreindexerconfig namespace "${blazegraph_namespace}"
+drush -y config-set --input-format=yaml triplestore_indexer.triplestoreindexerconfig method-of-op advanced_queue
+drush -y config-set --input-format=yaml triplestore_indexer.triplestoreindexerconfig aqj-max-retries 5
+drush -y config-set --input-format=yaml triplestore_indexer.triplestoreindexerconfig aqj-retry_delay 120
+drush -y config-set --input-format=yaml triplestore_indexer.triplestoreindexerconfig select-auth-method
+drush -y config-set --input-format=yaml triplestore_indexer.triplestoreindexerconfig advancedqueue-id triplestore
+drush -y config-set --input-format=yaml triplestore_indexer.triplestoreindexerconfig content-type-to-index "islandora_object: islandora_object"
+
+# configure fits
+drush -y config-set --input-format=yaml fits.fitsconfig fits-method "${fits_mode}"
+drush -y config-set --input-format=yaml fits.fitsconfig "${fits_config_var}" "${fits_url}"
+drush -y config-set --input-format=yaml fits.fitsconfig fits-advancedqueue_id fits
+drush -y config-set --input-format=yaml fits.fitsconfig fits-extract-ingesting 1
+drush -y config-set --input-format=yaml fits.fitsconfig aqj-max-retries  5
+drush -y config-set --input-format=yaml fits.fitsconfig aqj-retry_delay 120
+drush -y config-set --input-format=yaml fits.fitsconfig fits-default-fields "
+- field_fits_checksum
+- field_fits_file_format
+- field_fits
+"
