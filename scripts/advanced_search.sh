29201275304c1aa0eb2dd078dd110068c6730342
Kyle Huynh <7862086+kylehuynh205@users.noreply.github.com>
2022-04-04 15:50:09 -0400

    Update advanced_search.sh

--- scripts/advanced_search.sh
+++ scripts/advanced_search.sh
@@ -4,7 +4,7 @@
 inital_path=$PWD
 
 #current site
-site_path="${inital_path}"/..
+site_path=/var/www/drupal/
 
 # import advanced search configs
 drush -y config-import --partial --source="${site_path}"/islandora_lite_installation/configs/advanced_search
