ea5fd6f9c69f74525ef0912fab8d4cb6a31a9378
Natkeeran <natkeeran@gmail.com>
2022-06-02 11:10:55 -0400

    Update partial_config_sych.sh

--- scripts/partial_config_sych.sh
+++ scripts/partial_config_sych.sh
@@ -6,7 +6,7 @@ source $(dirname $0)/site_config.sh
 "$drush" -y config-import --partial --source="$islandora_lite_installation_path"/configs/advanced_search
 
 # rest config
-"$drush" -y config-import --partial --source="$islandora_lite_installation_path"/configs/rest_config
+"$drush" -y config-import --partial --source="$islandora_lite_installation_path"/configs/rest
 
 # media config
 "$drush" -y config-import --partial --source="$islandora_lite_installation_path"/configs/media_form_display
