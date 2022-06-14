c8d1e7f577c5d30518e414d998aaa5f0ea74a156
Natkeeran <natkeeran@gmail.com>
2022-06-14 09:44:18 -0400

    Update site_config.sh

--- scripts/site_config.sh
+++ scripts/site_config.sh
@@ -2,18 +2,19 @@
 
 #current site
 site_path=/var/www/drupal
+site_url="https://islandora.traefik.me"
 drush="$site_path"/vendor/drush/drush/drush
 islandora_lite_installation_path="$site_path/islandora_lite_installation"
 site_config_path="/var/www/drupal/dsu_sites_config"
 
 
 #configure_search_api_solr_module
-solr_host=http://islandora.traefik.me:8983/solr
+solr_host="$site_url":8983/solr
 solr_core=ISLANDORA
-cantaloupe_url=https://islandora.traefik.me/cantaloupe/iiif/2
+cantaloupe_url="$site_url"/cantaloupe/iiif/2
 
 #blazegraph
-blazegraph_url=https://islandora.traefik.me:8082/bigdata
+blazegraph_url="$site_url":8082/bigdata
 blazegraph_namespace=islandora
 
 #fits
