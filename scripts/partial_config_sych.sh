0318473cd32b3e6465959fdbc405c721443bc270
Natkeeran <natkeeran@gmail.com>
2022-06-14 14:30:25 -0400

    Update partial_config_sych.sh

--- scripts/partial_config_sych.sh
+++ scripts/partial_config_sych.sh
@@ -11,6 +11,9 @@ source $(dirname $0)/site_config.sh
 # media - overwrite default form display
 "$drush" -y config-import --partial --source="$islandora_lite_installation_path"/configs/media_form_display
 
+# media - overwrite default view display
+"$drush" -y config-import --partial --source="$islandora_lite_installation_path"/configs/media_view_display
+
 # simple config for rest_oai_pmh, citation_select
 "$drush" -y config-import --partial --source="$islandora_lite_installation_path"/configs/simple
 
