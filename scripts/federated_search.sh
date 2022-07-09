2f569063e33efe12f8c45dc7d5a115b89ecd5eee
Kyle Huynh <7862086+kylehuynh205@users.noreply.github.com>
2022-07-09 08:55:50 -0400

    Update federated_search.sh

--- scripts/federated_search.sh
+++ scripts/federated_search.sh
@@ -5,7 +5,7 @@
 
 source $(dirname $0)/site_config.sh
 
-[ -d "${site_path}"/dsu_sites_config ] && git clone https://github.com/digitalutsc/dsu_sites_config "${site_path}"/dsu_sites_config
+[ ! -d "${site_path}/dsu_sites_config" ] && git clone https://github.com/digitalutsc/dsu_sites_config "${site_path}"/dsu_sites_config
 
 # import config from federated search
 "$drush" -y config-import --partial --source="${site_path}"/dsu_sites_config/federated-search
